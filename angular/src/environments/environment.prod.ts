d4e8f93f3d42003fd693f42cfee48cb5777a029a
Bhanu Chowdary <bhanuphani100@gmail.com>
2020-03-28 19:26:43 +0530

    Add files via upload

---
+++ angular/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAyWysc8B6B0_yMd0OwkmRKOcqm4PBXsPA",
+    authDomain: "automathon-bot.firebaseapp.com",
+    databaseURL: "https://automathon-bot.firebaseio.com",
+    projectId: "automathon-bot",
+    storageBucket: "automathon-bot.appspot.com",
+    messagingSenderId: "577912689316",
+    appId: "1:577912689316:web:7cc042be3aef3b788f8c31"
+  }
+};
